5e7faa1745565a9be0b2e89674f6d22c46aa88b0
Rizwan <mdrizwan81@gmail.com>
2026-02-27 14:07:16 +0400

    updated 02/27/2026

---
+++ Reports/TEST/test report.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+using DevExpress.XtraReports.UI;
+
+namespace dxReports.Reports.TEST
+{
+    public partial class test_report : DevExpress.XtraReports.UI.XtraReport
+    {
+        public test_report()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
