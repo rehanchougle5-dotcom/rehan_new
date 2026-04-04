67945f9c95edfe6b4ab9dc42e3b8729a36622e7f
Rehan Chougle <rehanchougle5@gmail.com>
2026-04-04 14:27:20 +0400

    created packing list

---
+++ Reports/Packing List/Printing_List_Lanscape.cs
@@ -0,0 +1,17 @@
+﻿using DevExpress.XtraReports.UI;
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+
+namespace dxReports.Reports.Packing_List
+{
+    public partial class Printing_List_Lanscape : DevExpress.XtraReports.UI.XtraReport
+    {
+        public Printing_List_Lanscape()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
