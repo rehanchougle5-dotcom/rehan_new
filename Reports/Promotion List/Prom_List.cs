422a136a74d4a98f356ca823e991c91afe7887ea
Rehan Chougle <rehanchougle5@gmail.com>
2026-04-28 17:58:47 +0400

    created the promotionlist report

---
+++ Reports/Promotion List/Prom_List.cs
@@ -0,0 +1,17 @@
+﻿using DevExpress.XtraReports.UI;
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+
+namespace dxReports.Reports.Promotion_List
+{
+    public partial class Prom_List : DevExpress.XtraReports.UI.XtraReport
+    {
+        public Prom_List()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
